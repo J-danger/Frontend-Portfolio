0c470b77b61e673a68fe369e03783dd73529a123
Stephen Howe <Stevehowe10@gmail.com>
2020-04-06 20:58:38 -0500

    we have waves

--- run.js
+++ run.js
@@ -1,3 +1,18 @@
+
+function home() {        
+    var x = document.getElementById("intro");
+    var y = document.getElementById("projects")
+    if (x.style.display === "none") {
+        x.style.display = "inline-block";  
+        y.style.display = "none"      
+    } 
+}
+
 function projects() {
-    alert('test')
-  }
+    var x = document.getElementById("projects");
+    var y = document.getElementById("intro")
+    if (x.style.display === "none") {
+        x.style.display = "inline-block";  
+        y.style.display = "none"      
+    } 
+}
