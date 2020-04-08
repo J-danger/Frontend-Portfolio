bf8a9973ec23f2796b13bbb4a724bb7ad1999ffa
Stephen Howe <Stevehowe10@gmail.com>
2020-04-08 17:57:48 -0500

    night time

--- run.js
+++ run.js
@@ -1,36 +1,62 @@
 
 function home() {        
-    var x = document.getElementById("intro");
-    var y = document.getElementById("projects")
-    var z = document.getElementById("polygon_path")
+    var intro = document.getElementById("intro");
+    var projects = document.getElementById("projects")
+    var polygon = document.getElementById("polygon_path")
     var sun = document.getElementById("sun")
-    if (x.style.display === "none") {
-        x.style.display = "inline-block";  
-        y.style.display = "none"      
+    if (intro.style.display === "none") {
+        intro.style.display = "inline-block";  
+        projects.style.display = "none"      
     } 
-    z.style.backgroundColor = "rgb(70,164,213)"
-    z.style.background = "radial-gradient(circle, rgba(70,164,213,1) 0%, rgba(0,160,186,1) 61%)"
-    z.style.transition = "2s"
+    polygon.style.backgroundColor = "rgb(70,164,213)"
+    polygon.style.background = "radial-gradient(circle, rgba(70,164,213,1) 0%, rgba(0,160,186,1) 61%)"
+    
 
     sun.style.top = "75px"
     sun.style.left= "50px"
+    sun.style.backgroundColor = "yellow"
+    sun.style.boxShadow = "0 0 30px 15px #fff, 0 0 50px 30px orangered, 0 0 70px 45px yellow"
 }
 
 function projects() {
-    var x = document.getElementById("projects");
-    var y = document.getElementById("intro")
-    var z = document.getElementById("polygon_path")
+    var intro = document.getElementById("intro")
+    var projects = document.getElementById("projects");
+    var polygon = document.getElementById("polygon_path")
     var sun = document.getElementById("sun")
-    if (x.style.display === "none") {
-        x.style.display = "inline-block";  
-        y.style.display = "none"      
+    if (projects.style.display === "none") {
+        projects.style.display = "inline-block";  
+        intro.style.display = "none"      
     } 
-    z.style.backgroundColor = "rgb(235,201,65)"
-    z.style.background = "radial-gradient(circle, rgba(235,201,65,1) 0%, rgba(242,118,46,1) 43%)"
-    z.style.transition = "2s"
+    polygon.style.backgroundColor = "rgb(235,201,65)"
+    polygon.style.background = "radial-gradient(circle, rgba(235,201,65,1) 0%, rgba(242,118,46,1) 43%)"
+    
+   
     sun.style.top = "60%"
     sun.style.left = "55%"
+    sun.style.backgroundColor = "yellow"
+    sun.style.boxShadow = "0 0 30px 15px #fff, 0 0 50px 30px orangered, 0 0 70px 45px yellow"
     sun.animation = "sun"
+}
+
+function contact() {
+    var into = document.getElementById("intro")
+    var projects = document.getElementById("projects");
+    var polygon = document.getElementById("polygon_path")
+    var sun = document.getElementById("sun")
+    if (projects.style.display === "none") {
+        projects.style.display = "inline-block";  
+        intro.style.display = "none"      
+    } 
+    // polygon.style.backgroundColor = "rgb(235,201,65)"
+    polygon.style.background = "#000 url(http://www.script-tutorials.com/demos/360/images/stars.png)"
+    
+   
+    sun.style.top = "75px"
+    sun.style.left = "50px"
+    sun.style.backgroundColor = "white"
+    sun.style.boxShadow = "none"
+    sun.style.transitionDuration = "2s"
+    sun.animation = "moon"
 }
 
 function test(){
